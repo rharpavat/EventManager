f0d218dc5f01bcb61ba7f345406932d4e51081d2
Rishu Harpavat <rishuharpavat@gmail.com>
2018-10-29 23:32:21 -0500

    Add models and admin console

--- EventManager/event_manager/admin.py
+++ EventManager/event_manager/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Crawl
 
 # Register your models here.
+
+admin.site.register(Crawl)
--- EventManager/event_manager/models.py
+++ EventManager/event_manager/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Crawl(models.Model):
+    """
+    Simple model for a crawl.
+    """
+
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=255, null=False, blank=False)
+    date = models.DateField(null=True, blank=True)
+
+
+class CrawlStop(models.Model):
+    """
+    Simple model for a crawl stop.
+    """
+
+    crawl_id = models.ForeignKey(Crawl, on_delete=models.CASCADE)
+    number = models.IntegerField(null=False, blank=False, unique=True)
+    location = models.CharField(max_length=255, null=True, blank=True)
+    hosts = models.CharField(max_length=255, null=False, blank=False)
+    drink = models.CharField(max_length=255, null=False, blank=True)
